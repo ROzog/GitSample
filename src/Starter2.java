3b277a609773a0fa937f8e582e73e87e22756b3a
Robert Ozog <info@robertozog.com>
2018-11-12 11:55:33 +0100

    nowa metoda

--- src/Starter2.java
+++ src/Starter2.java
@@ -1,6 +1,10 @@
 public class Starter2 {
     public static void main(String[] args) {
         System.out.println("dodałem");
+        printText("nowa metodka");
+    }
 
+    public static void printText(String text) {
+        System.out.println(text);
     }
 }
