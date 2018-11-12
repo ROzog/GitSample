a73a224f899d44dede4eb85869892a880359bc15
Robert Ozog <info@robertozog.com>
2018-11-12 12:17:08 +0100

    zmiany zmiany zmiany

--- src/Starter2.java
+++ src/Starter2.java
@@ -1,15 +1,20 @@
 public class Starter2 {
     public static void main(String[] args) {
         int age =33;
+        int age2 = 55;
         String name = "Tomasz";
         boolean prawda = true;
         System.out.println("dodałem");
         printText("nowa metodka");
         System.out.println("jakies nowe zmiany");
+        System.out.println("zmiany zmiany zmiany");
     }
 
     public static void printText(String text) {
         System.out.println(text);
+    }
+    public static int add(int a, int b){
+        return a+b;
     }
 
 
