10105de3c025c7a77eb579eb595d447b2d228f6d
Robert Ozog <info@robertozog.com>
2018-11-12 12:06:46 +0100

    kilka zmiennych i jeden sout

--- src/Starter2.java
+++ src/Starter2.java
@@ -1,10 +1,16 @@
 public class Starter2 {
     public static void main(String[] args) {
+        int age =33;
+        String name = "Tomasz";
+        boolean prawda = true;
         System.out.println("dodałem");
         printText("nowa metodka");
+        System.out.println("jakies nowe zmiany");
     }
 
     public static void printText(String text) {
         System.out.println(text);
     }
+
+
 }
