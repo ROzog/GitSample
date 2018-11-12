f49d25b070df3d87d49f60e67b13bc169c372305
Robert Ozog <info@robertozog.com>
2018-11-12 10:14:33 +0100

    adding method

--- src/Starter.java
+++ src/Starter.java
@@ -2,4 +2,7 @@ public class Starter {
     public static void main(String[] args) {
         System.out.println("Welcome to the Git");
     }
+    public static void printSomething (String text){
+        System.out.println(text);
+    }
 }
