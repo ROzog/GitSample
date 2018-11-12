aacd3fae6b8429b43093792471ca0884aa8781d2
Jakub Modras <kuba.mod@onet.pl>
2018-11-12 12:07:26 +0100

    zmiana

--- src/Starter2.java
+++ src/Starter2.java
@@ -2,9 +2,13 @@ public class Starter2 {
     public static void main(String[] args) {
         System.out.println("dodałem");
         printText("nowa metodka");
+        someText("hemoglobina taka sytuacja");
     }
 
     public static void printText(String text) {
         System.out.println(text);
+    }
+    public static void someText(String someText){
+        System.out.println(someText);
     }
 }
